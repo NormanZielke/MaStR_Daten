99fa8ad35175b6f0d9c51ee103a1b0f447da401b
NormanZielke <nzielke@gmx.net>
2024-05-28 17:15:13 +0200

    Get Topologie of combustion and biomass

--- functions.py
+++ functions.py
@@ -3,10 +3,12 @@ import pickle
 
 # import dictionary for ags_id's
 
-def filter_by_reg(df):
-    with open("gemeindeschluessel.pkl", "rb") as datei:
-        gemeindeschluessel = pickle.load(datei)
-        regions = gemeindeschluessel.keys()
+def filter_by_reg(df,regions):
+    """
+    :param df: pandas dataframe
+    :param regions: dtype: string array
+    :return: df_f: pandas dataframe
+    """
     df = df.loc[df["EinheitBetriebsstatus"] == "In Betrieb"] # pick only plants which are currently running
     con = []
     for region in regions:
@@ -14,4 +16,13 @@ def filter_by_reg(df):
     df_f = pd.concat(con)
     return df_f
 
-
+def topologie(df):
+    """
+    :param df: pandas dataframe
+    :return capacities: pandas dataframe
+    """
+    capacities_t = df.groupby(["Technologie", "Hauptbrennstoff"]).ThermischeNutzleistung.agg(["sum", "count"]).reset_index()
+    capacities_b = df.groupby(["Technologie", "Hauptbrennstoff"]).Bruttoleistung.sum().reset_index()
+    capacities = pd.merge(capacities_b, capacities_t)
+    capacities.rename(columns={"Bruttoleistung": "Bruttoleistung [kW]", "sum": "Thermische Nutzleistung [kW]", "count": "Anzahl"}, inplace=True)
+    return capacities
--- mastr.py
+++ mastr.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from functions import filter_by_reg
+from functions import filter_by_reg, topologie
 
 """
 pd.set_option('display.max_rows', None)
@@ -46,29 +46,92 @@ gsgk = pd.read_csv("csv/bnetza_mastr_gsgk_raw.csv",
 # --------------------------------------------------------------------------------------------------------------------->
 # filter raw data by Municipalities
 
-df_comb = filter_by_reg(combustion)
-df_biomass = filter_by_reg(biomass)
-df_gsgk = filter_by_reg(gsgk) # --> only 2 powerplants
+regions = gemeindeschluessel.keys()
+
+df_comb = filter_by_reg(combustion, regions)
+df_biomass = filter_by_reg(biomass, regions)
+df_gsgk = filter_by_reg(gsgk, regions) # --> only 2 power plants
 
 # --------------------------------------------------------------------------------------------------------------------->
 # summarize data by technologies with their fueltype
 
-capacities_comb = df_comb.groupby(["Gemeinde","Technologie","Hauptbrennstoff"]).Bruttoleistung.sum()
-capacities_biomass = df_biomass.groupby(["Gemeinde","Technologie","Hauptbrennstoff"]).Bruttoleistung.sum()
+capacities_comb = df_comb.groupby(["Gemeinde", "Technologie", "Hauptbrennstoff"]).Bruttoleistung.sum()
+capacities_biomass = df_biomass.groupby(["Gemeinde", "Technologie", "Hauptbrennstoff"]).Bruttoleistung.sum()
+
+# filter dataframe "combustion" to create dataframe for every region
+
+# combustion
+df_comb_Strausberg = combustion.loc[combustion.Gemeinde == "Strausberg"]
+df_comb_Erkner = combustion.loc[combustion.Gemeinde == "Erkner"]
+df_comb_Ruedersdorf = combustion.loc[combustion.Gemeinde == "Rüdersdorf bei Berlin"]
+df_comb_GruenH = combustion.loc[combustion.Gemeinde == "Grünheide (Mark)"]
+df_comb_Bocholt = combustion.loc[combustion.Gemeinde == "Bocholt"]
+df_comb_Zwickau = combustion.loc[combustion.Gemeinde == "Zwickau"]
+df_comb_Ingolstadt = combustion.loc[combustion.Gemeinde == "Ingolstadt"]
+df_comb_Kassel = combustion.loc[combustion.Gemeinde == "Kassel"]
+df_comb_Kiel = combustion.loc[combustion.Gemeinde == "Kiel"]
+
+# filter dataframe "biomass" to create dataframe for every region
+
+# biomass
+
+df_biomass_Strausberg = biomass.loc[biomass.Gemeinde == "Strausberg"]
+df_biomass_Erkner = biomass.loc[biomass.Gemeinde == "Erkner"]
+df_biomass_Ruedersdorf = biomass.loc[biomass.Gemeinde == "Rüdersdorf bei Berlin"]
+df_biomass_GruenH = biomass.loc[biomass.Gemeinde == "Grünheide (Mark)"]
+df_biomass_Bocholt = biomass.loc[biomass.Gemeinde == "Bocholt"]
+df_biomass_Zwickau = biomass.loc[biomass.Gemeinde == "Zwickau"]
+df_biomass_Ingolstadt = biomass.loc[biomass.Gemeinde == "Ingolstadt"]
+df_biomass_Kassel = biomass.loc[biomass.Gemeinde == "Kassel"]
+df_biomass_Kiel = biomass.loc[biomass.Gemeinde == "Kiel"]
+
+# filter dataframes of regions by technologie with their capacities
+
+# combustion
+
+capacities_comb_Strausberg = topologie(df_comb_Strausberg)
+capacities_comb_Erkner = topologie(df_comb_Erkner)
+capacities_comb_Ruedersdorf = topologie(df_comb_Ruedersdorf)
+capacities_comb_GruenH = topologie(df_comb_GruenH)
+capacities_comb_Bocholt = topologie(df_comb_Bocholt)
+capacities_comb_Zwickau = topologie(df_comb_Zwickau)
+capacities_comb_Ingolstadt = topologie(df_comb_Ingolstadt)
+capacities_comb_Kassel = topologie(df_comb_Kassel)
+capacities_comb_Kiel = topologie(df_comb_Kiel)
+
+# biomass
+
+capacities_biomass_Strausberg = topologie(df_biomass_Strausberg)
+capacities_biomass_Erkner = topologie(df_biomass_Erkner)
+capacities_biomass_Ruedersdorf = topologie(df_biomass_Ruedersdorf)
+capacities_biomass_GruenH = topologie(df_biomass_GruenH)
+capacities_biomass_Bocholt = topologie(df_biomass_Bocholt)
+capacities_biomass_Zwickau = topologie(df_biomass_Zwickau)
+capacities_biomass_Ingolstadt = topologie(df_biomass_Ingolstadt)
+capacities_biomass_Kassel = topologie(df_biomass_Kassel)
+capacities_biomass_Kiel = topologie(df_biomass_Kiel)
 
 # --------------------------------------------------------------------------------------------------------------------->
-# show data
+# Output
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
+with pd.ExcelWriter("360_SLE_Topologie_Combustion.xlsx") as writer:
+    capacities_comb_Strausberg.to_excel(writer, sheet_name="Strausberg", index=False)
+    capacities_comb_Erkner.to_excel(writer, sheet_name="Erkner", index=False)
+    capacities_comb_Ruedersdorf.to_excel(writer, sheet_name="Rüdersdorf bei Berlin", index=False)
+    capacities_comb_GruenH.to_excel(writer, sheet_name="Grünheide (Mark)", index=False)
+    capacities_comb_Bocholt.to_excel(writer, sheet_name="Bocholt", index=False)
+    capacities_comb_Zwickau.to_excel(writer, sheet_name="Zwickau", index=False)
+    capacities_comb_Ingolstadt.to_excel(writer, sheet_name="Ingolstadt", index=False)
+    capacities_comb_Kassel.to_excel(writer, sheet_name="Kassel", index=False)
+    capacities_comb_Kiel.to_excel(writer, sheet_name="Kiel", index=False)
 
-print("bnetza_mastr_combustion_raw.csv")
-print(capacities_comb)
-print("bnetza_mastr_biomass_raw.csv")
-print(capacities_biomass)
-
-pd.reset_option('display.max_rows')
-pd.reset_option('display.max_columns')
-
-print("bnetza_mastr_gdgk_raw.csv")
-print(df_gsgk.loc[:,["Gemeinde","Technologie","Bruttoleistung"]])
+with pd.ExcelWriter("360_SLE_Topologie_biomass.xlsx") as writer:
+    capacities_biomass_Strausberg.to_excel(writer, sheet_name="Strausberg", index=False)
+    capacities_biomass_Erkner.to_excel(writer, sheet_name="Erkner", index=False)
+    capacities_biomass_Ruedersdorf.to_excel(writer, sheet_name="Rüdersdorf bei Berlin", index=False)
+    capacities_biomass_GruenH.to_excel(writer, sheet_name="Grünheide (Mark)", index=False)
+    capacities_biomass_Bocholt.to_excel(writer, sheet_name="Bocholt", index=False)
+    capacities_biomass_Zwickau.to_excel(writer, sheet_name="Zwickau", index=False)
+    capacities_biomass_Ingolstadt.to_excel(writer, sheet_name="Ingolstadt", index=False)
+    capacities_biomass_Kassel.to_excel(writer, sheet_name="Kassel", index=False)
+    capacities_biomass_Kiel.to_excel(writer, sheet_name="Kiel", index=False)
